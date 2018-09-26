5ba5a6382820de77029038c0eb0491190a689aa9
Brendan J. Baker <brendan@brendanbaker.com>
2018-09-26 00:06:31 -0400

    Attempt to clean up even after publish failure.

--- scripts/publish-remote.sh
+++ scripts/publish-remote.sh
@@ -8,14 +8,38 @@ path_root=$(dirname $(dirname "$path_script"))
 project=jbakery-extensions-spring-social
 branch_temporary=temp/$publish_id
 
-pushd $path_root
-branch_original=$(git symbolic-ref --short HEAD)
-git checkout -b "$branch_temporary"
-pushd $path_root/$project
-export GPG_TTY=$(tty)
-export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
-mvn release:clean release:prepare release:clean
-popd
-git checkout "$branch_original"
-git branch -D "$branch_temporary"
-popd
+function execute {
+	# Check out temporary branch.
+
+	pushd $path_root
+	branch_original=$(git symbolic-ref --short HEAD)
+	git checkout -b "$branch_temporary"
+	popd
+
+	# Build.
+
+	pushd $path_root/$project
+	export GPG_TTY=$(tty)
+	export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+	mvn release:clean release:prepare release:clean
+	popd
+
+	# Cleanup.
+
+	cleanup
+}
+
+function cleanup {
+	echo "Original branch: $branch_original"
+	echo "Temporary branch: $branch_temporary"
+	pushd $path_root
+	git checkout "$branch_original"
+	git branch -D "$branch_temporary"
+	popd
+}
+
+set +e
+execute
+set -e
+
+cleanup
