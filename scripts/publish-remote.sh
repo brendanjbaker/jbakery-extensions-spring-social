de79f4aade6a939f366f5dc3262c12cb35e8b99b
Brendan J. Baker <brendan@brendanbaker.com>
2018-09-25 23:59:21 -0400

    Remote deployment script improvements.

--- scripts/publish-remote.sh
+++ scripts/publish-remote.sh
@@ -2,12 +2,20 @@
 
 set -euxo pipefail
 
+publish_id=$(openssl rand -hex 16)
 path_script=$(readlink -f "$0")
 path_root=$(dirname $(dirname "$path_script"))
 project=jbakery-extensions-spring-social
+branch_temporary=temp/$publish_id
 
+pushd $path_root
+branch_original=$(git symbolic-ref --short HEAD)
+git checkout -b "$branch_temporary"
 pushd $path_root/$project
 export GPG_TTY=$(tty)
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 mvn release:clean release:prepare release:clean
+popd
+git checkout "$branch_original"
+git branch -D "$branch_temporary"
 popd
